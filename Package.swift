c9dced72a81bdb824293e2a6ff89095c71434c33
chitrakotwani <chitra.kk18@gmail.com>
2018-02-08 15:30:03 +0000

    Add dependance of RxAlamofire on RxCocoa as well

--- Package.swift
+++ Package.swift
@@ -24,11 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxAlamofire",
-            dependencies: ["RxSwift", "Alamofire"],
+            dependencies: ["RxSwift", "Alamofire", "RxCocoa"],
             path: "Sources"),
         .testTarget(
             name: "RxAlamofireTests",
-            dependencies: ["RxAlamofire", "RxSwift", "Alamofire"],
+            dependencies: ["RxAlamofire", "RxSwift", "Alamofire", "RxCocoa"],
             path:"RxAlamofire/RxAlamofireTests"),
     ]
 )
